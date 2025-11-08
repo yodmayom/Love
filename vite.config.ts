d819d9154e6c81168a279e05c88b6b18b91b04ce
Yodmayom <Yodmayom3@gmail.com>
2025-11-08 19:52:03 +0700

    FIX2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // ✅ ปรับให้เรียบง่าย ใช้งานได้แน่นอนบน Vercel
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/',
+  base: process.env.VITE_BASE_PATH || '/Love',
 })
