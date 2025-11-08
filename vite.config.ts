7a517b84050e1b6a25f460b91143e367995ffd79
Yodmayom <Yodmayom3@gmail.com>
2025-11-08 19:49:36 +0700

    FIX

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
+  base: process.env.VITE_BASE_PATH || '/Love',
   plugins: [
     react({
       babel: {
